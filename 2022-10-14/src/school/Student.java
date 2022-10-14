4cf8215de6f93c763a0cad36db4866edeaad0302
Cheongpark(청박) <55844749+cheongpark@users.noreply.github.com>
2022-10-14 11:02:39 +0900

    2022-10-14

---
+++ 2022-10-14/src/school/Student.java
@@ -0,0 +1,54 @@
+package school;
+
+import java.util.ArrayList;
+
+public class Student {
+    private int studentID;
+    private String studentName;
+    private Subject majorSubject;
+    private ArrayList<Score> scoreList = new ArrayList<Score>();
+
+    public Student(int studentID, String studentName, Subject majorSubject) {
+        this.studentID = studentID;
+        this.studentName = studentName;
+        this.majorSubject = majorSubject;
+    }
+    
+    //Getter Setter
+    public int getStudentID() {
+        return studentID;
+    }
+    
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+    
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName() {
+        this.studentName = studentName;
+    }
+
+    public Subject getMajorSubject() {
+        return majorSubject;
+    }
+
+    public void setMajorSubject(Subject majorSubject) {
+        this.majorSubject = majorSubject;
+    }
+
+    public ArrayList<Score> getScoreList() {
+        return scoreList;
+    }
+
+    public void setScoreList(ArrayList<Score> scoreList) {
+        this.scoreList = scoreList;
+    }
+
+    //Method
+    public void addSubjectScore(Score score) {
+        scoreList.add(score);
+    }
+}
