eed04701b413c5b37f9f392714904997ffb42b76
Cheongpark(청박) <55844749+cheongpark@users.noreply.github.com>
2022-10-17 16:06:54 +0900

    2022-10-17

---
+++ 2022-10-17/src/school/Subject.java
@@ -0,0 +1,48 @@
+package school;
+
+import utils.Define;
+import grade.GradeEvaluation;
+import java.util.ArrayList;
+
+public class Subject {
+    private int subjectID;
+    private String subjectName;
+    private int gradeType;
+    ArrayList<Student> studentList = new ArrayList<Student>();
+
+    public Subject(String subjectName, int subjectID) {
+        this.subjectName = subjectName;
+        this.subjectID = subjectID;
+    }
+
+    //Getter Setter
+    public int getSubjectID() {
+        return subjectID;
+    }
+
+    public void setSubjectID(int subjectID) {
+        this.subjectID = subjectID;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public int getGradeType() {
+        return gradeType;
+    }
+
+    public void setGradeType(int gradeType) {
+        this.gradeType = gradeType;
+    }
+
+    //Method
+    public void register(Student student) {
+        studentList.add(student);
+    }
+
+}
